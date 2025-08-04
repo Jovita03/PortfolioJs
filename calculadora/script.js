0a923ed5b2138f61dad540f559ec787f28678b1f
Jesenia <jesscortez1594@gmail.com>
2025-08-03 22:38:18 -0600

    Comments

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,17 +1,17 @@
-let display = document.getElementById('display');
+let display = document.getElementById('display'); //busca en el html el elemento con el atributo id="display"
 
-function agregar(valor) {
-  display.value += valor;
+function agregar(valor) { //decvlara la funcion y recibe un valor llamado valor 
+  display.value += valor; // concatena el valor al contenido actual del display
 }
 
 function borrar() {
-  display.value = '';
+  display.value = '';// borra el contenido del display asignando una cadena vacia 
 }
 
 function calcular() {
-  try {
+  try {//maneja errores en caso de que la expresión no sea válida
     display.value = eval(display.value);
-  } catch {
+  } catch {// si  la entrada es invalida solo regresa el error
     display.value = 'Error';
   }
 }
